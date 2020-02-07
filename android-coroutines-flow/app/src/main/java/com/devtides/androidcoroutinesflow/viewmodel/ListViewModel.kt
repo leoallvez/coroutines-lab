67323d7d42f94f3e803591683ef9a1ded5e3a7ae
Leonardo <leoallvez@hotmail.com>
2020-02-06 23:59:33 -0300

    android coroutines flow

---
+++ android-coroutines-flow/app/src/main/java/com/devtides/androidcoroutinesflow/viewmodel/ListViewModel.kt
@@ -0,0 +1,11 @@
+package com.devtides.androidcoroutinesflow.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.devtides.androidcoroutinesflow.model.NewsRepository
+
+class ListViewModel: ViewModel() {
+
+    val newsArticles = NewsRepository().getNewsArticle().asLiveData()
+
+}
